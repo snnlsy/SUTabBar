43950b7f1db8a41fbd9a886489e65a9507248ba6
Sinan Ulusoy <ssinanulusoy@gmail.com>
2024-02-08 17:35:25 +0300

    Add package

---
+++ Sources/SUTabBar/Internal/SUSpacing.swift
@@ -0,0 +1,17 @@
+//
+//  SUSpacing.swift
+//  SUTabBar
+//
+//  Created by Sinan Ulusoy on 6.12.2023.
+//
+
+import Foundation
+
+
+// MARK: - SUSpacing
+
+enum SUSpacing {
+    
+    static let small: CGFloat = 6
+    static let xxLarge: CGFloat = 16
+}
---
+++ Sources/SUTabBar/UITabBar+Extension.swift
@@ -0,0 +1,38 @@
+//
+//  UITabBar+Extension.swift
+//  SUTabBar
+//
+//  Created by Sinan Ulusoy on 7.12.2023.
+//
+
+import UIKit
+
+
+// MARK: - UITabBar Open Properties
+
+extension UITabBar {
+
+    @objc
+    open var indicatorColor: UIColor? {
+        get { nil }
+        set {}
+    }
+    
+    @objc
+    open var iconColor: UIColor? {
+        get { nil }
+        set {}
+    }
+    
+    @objc
+    open var titleColor: UIColor? {
+        get { nil }
+        set {}
+    }
+    
+    @objc
+    open var titleFont: UIFont? {
+        get { nil }
+        set {}
+    }
+}
